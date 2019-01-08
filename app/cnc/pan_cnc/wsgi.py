a09aca737943916b24ba4fad5691a66e23c808b1
Nathan <nembery@gmail.com>
2019-01-08 10:04:35 -0500

    move to cnc views first cut

--- app/cnc/pan_cnc/wsgi.py
+++ app/cnc/pan_cnc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vistoq project.
+WSGI config for pan_cnc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vistoq.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pan_cnc.settings')
 
 application = get_wsgi_application()
--- app/mssp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MSSPConfig(AppConfig):
-    name = 'mssp'
-
